8cb02de5c44e54e2ce878b2b4bbc8fa9feed0797
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-10 16:31:35 +1030

    adding stuff

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -4,7 +4,7 @@ python main_moco.py \
 --gpu=0 \
 --data="/jvrielink/AIML_rot_corrected" \
 --annotations_file="annotations/img_paths_mini.csv" \
--b=512 \
+-b=256 \
 --epochs=8 \
 --workers=14 \
 --exp_name="debug_moco" \
