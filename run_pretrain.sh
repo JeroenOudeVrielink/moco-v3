68fbb9d863071439b2de3a7a12ee2a2a103718c4
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-10 16:43:25 +1030

    mod script

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -3,13 +3,12 @@ python main_moco.py \
 --crop-min=.2 \
 --gpu=0 \
 --data="/jvrielink/AIML_rot_corrected" \
---annotations_file="annotations/img_paths_mini.csv" \
--b=256 \
---epochs=8 \
+--annotations_file="annotations/img_paths.csv" \
+-b=512 \
+--epochs=400 \
 --workers=14 \
 --exp_name="debug_moco" \
---output_dir="/data/moco_models" \
---model_ckpt_freq=2
+--output_dir="/data/moco_models"
 
 
 
