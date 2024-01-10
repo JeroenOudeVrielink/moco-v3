b98ae03ddfd27956363647468c7ef35cbd810e84
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-10 16:27:36 +1030

    adding debug stuff

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -4,11 +4,12 @@ python main_moco.py \
 --gpu=0 \
 --data="/jvrielink/AIML_rot_corrected" \
 --annotations_file="annotations/img_paths_mini.csv" \
--b=256 \
+-b=512 \
 --epochs=8 \
 --workers=14 \
 --exp_name="debug_moco" \
---output_dir="data/moco_models"
+--output_dir="/data/moco_models" \
+--model_ckpt_freq=2
 
 
 
